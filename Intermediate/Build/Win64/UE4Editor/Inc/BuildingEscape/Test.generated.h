59527c0cd59368d6e3e3d9c0c37b11406c2f9258
Zagorouiko <Zagorouiko@gmail.com>
2019-02-13 11:49:12 -0800

    Add project files.

---
+++ Intermediate/Build/Win64/UE4Editor/Inc/BuildingEscape/Test.generated.h
@@ -0,0 +1,88 @@
+// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef BUILDINGESCAPE_Test_generated_h
+#error "Test.generated.h already included, missing '#pragma once' in Test.h"
+#endif
+#define BUILDINGESCAPE_Test_generated_h
+
+#define BuildingEscape_Source_BuildingEscape_Test_h_12_RPC_WRAPPERS
+#define BuildingEscape_Source_BuildingEscape_Test_h_12_RPC_WRAPPERS_NO_PURE_DECLS
+#define BuildingEscape_Source_BuildingEscape_Test_h_12_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesATest(); \
+	friend struct Z_Construct_UClass_ATest_Statics; \
+public: \
+	DECLARE_CLASS(ATest, AActor, COMPILED_IN_FLAGS(0), CASTCLASS_None, TEXT("/Script/BuildingEscape"), NO_API) \
+	DECLARE_SERIALIZER(ATest)
+
+
+#define BuildingEscape_Source_BuildingEscape_Test_h_12_INCLASS \
+private: \
+	static void StaticRegisterNativesATest(); \
+	friend struct Z_Construct_UClass_ATest_Statics; \
+public: \
+	DECLARE_CLASS(ATest, AActor, COMPILED_IN_FLAGS(0), CASTCLASS_None, TEXT("/Script/BuildingEscape"), NO_API) \
+	DECLARE_SERIALIZER(ATest)
+
+
+#define BuildingEscape_Source_BuildingEscape_Test_h_12_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API ATest(const FObjectInitializer& ObjectInitializer); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(ATest) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, ATest); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ATest); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API ATest(ATest&&); \
+	NO_API ATest(const ATest&); \
+public:
+
+
+#define BuildingEscape_Source_BuildingEscape_Test_h_12_ENHANCED_CONSTRUCTORS \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API ATest(ATest&&); \
+	NO_API ATest(const ATest&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, ATest); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ATest); \
+	DEFINE_DEFAULT_CONSTRUCTOR_CALL(ATest)
+
+
+#define BuildingEscape_Source_BuildingEscape_Test_h_12_PRIVATE_PROPERTY_OFFSET
+#define BuildingEscape_Source_BuildingEscape_Test_h_9_PROLOG
+#define BuildingEscape_Source_BuildingEscape_Test_h_12_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	BuildingEscape_Source_BuildingEscape_Test_h_12_PRIVATE_PROPERTY_OFFSET \
+	BuildingEscape_Source_BuildingEscape_Test_h_12_RPC_WRAPPERS \
+	BuildingEscape_Source_BuildingEscape_Test_h_12_INCLASS \
+	BuildingEscape_Source_BuildingEscape_Test_h_12_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define BuildingEscape_Source_BuildingEscape_Test_h_12_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	BuildingEscape_Source_BuildingEscape_Test_h_12_PRIVATE_PROPERTY_OFFSET \
+	BuildingEscape_Source_BuildingEscape_Test_h_12_RPC_WRAPPERS_NO_PURE_DECLS \
+	BuildingEscape_Source_BuildingEscape_Test_h_12_INCLASS_NO_PURE_DECLS \
+	BuildingEscape_Source_BuildingEscape_Test_h_12_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID BuildingEscape_Source_BuildingEscape_Test_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
