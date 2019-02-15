68c53c7717fa1009bbc36d4b4fbc21f20aeaa984
Zagorouiko <Zagorouiko@gmail.com>
2019-02-15 07:05:42 -0800

    Added position reporter component

--- Intermediate/Build/Win64/UE4Editor/Inc/BuildingEscape/BuildingEscape.init.gen.cpp
+++ Intermediate/Build/Win64/UE4Editor/Inc/BuildingEscape/BuildingEscape.init.gen.cpp
@@ -19,8 +19,8 @@ void EmptyLinkFunctionForGeneratedCodeBuildingEscape_init() {}
 			static const UE4CodeGen_Private::FPackageParams PackageParams = {
 				"/Script/BuildingEscape",
 				PKG_CompiledIn | 0x00000000,
-				0xA5A32E60,
-				0xDEA99335,
+				0x8B533665,
+				0xE2D146B8,
 				nullptr, 0,
 				METADATA_PARAMS(nullptr, 0)
 			};
